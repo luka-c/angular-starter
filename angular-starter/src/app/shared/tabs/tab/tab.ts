d450ef3a6470f47b0496ab971345a315787adc21
Luka Cicak <cicak.luka@gmail.com>
2025-07-11 14:31:57 +0200

    Started tabs

--- angular-starter/src/app/shared/tabs/tab/tab.ts
+++ angular-starter/src/app/shared/tabs/tab/tab.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input, TemplateRef, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   imports: [],
   templateUrl: './tab.html',
-  styleUrl: './tab.scss'
+  styleUrl: './tab.scss',
 })
 export class Tab {
+  /**
+   * The value of the tab.
+   */
+  readonly value = input<string>();
 
+  /**
+   * The label of the tab.
+   */
+  readonly label = input<string>();
+
+  /**
+   * The content of the tab.
+   */
+  readonly content = viewChild.required<TemplateRef<void>>('content');
 }
--- angular-starter/src/app/shared/tabs/tabs.ts
+++ angular-starter/src/app/shared/tabs/tabs.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, contentChildren, model } from '@angular/core';
+import {
+  NgpTabButton,
+  NgpTabList,
+  NgpTabPanel,
+  NgpTabset,
+} from 'ng-primitives/tabs';
+import { Tab } from './tab/tab';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
+  imports: [NgpTabset, NgpTabButton, NgpTabList, NgpTabPanel, NgTemplateOutlet],
   templateUrl: './tabs.html',
-  styleUrl: './tabs.scss'
+  styleUrl: './tabs.scss',
 })
 export class Tabs {
+  /**
+   * The value of the selected tab.
+   */
+  readonly value = model<string>();
 
+  /**
+   * The tabs in the group.
+   */
+  readonly tabs = contentChildren(Tab);
 }
